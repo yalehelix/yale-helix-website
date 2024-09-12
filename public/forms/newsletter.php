b23034da732f3947b113c5a1c2c0b16810463b53
William Liu <william.liu@yale.edu>
2024-09-12 19:03:06 -0400

    contact and newsletter php files

---
+++ public/forms/newsletter.php
@@ -0,0 +1,21 @@
+<?php
+  $receiving_email_address = 'admin@yalehelix.org';
+
+  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
+    include( $php_email_form );
+  } else {
+    die( 'Unable to load the "PHP Email Form" Library!');
+  }
+
+  $contact = new PHP_Email_Form;
+  $contact->ajax = true;
+  
+  $contact->to = $receiving_email_address;
+  $contact->from_name = $_POST['email'];
+  $contact->from_email = $_POST['email'];
+  $contact->subject ="New Subscription: " . $_POST['email'];
+
+  $contact->add_message( $_POST['email'], 'Email');
+
+  echo $contact->send();
+?>
